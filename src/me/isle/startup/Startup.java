8129a99b1180fc130e116e62df620912b75f9088
Vemahk <Vemahk20@gmail.com>
2018-04-12 04:55:35 -0500

    Initial Commit.

---
+++ src/me/isle/startup/Startup.java
@@ -0,0 +1,11 @@
+package me.isle.startup;
+
+import java.io.IOException;
+
+public class Startup {
+
+	public static void main(String[] args) throws IOException{
+		System.out.println("Hello World!");
+	}
+	
+}
